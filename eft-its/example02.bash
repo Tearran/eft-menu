f4c3bfa135632dc09efcde6650c7c24f17860454
Joseph Turner <Tearran@users.noreply.github.com>
2022-05-30 10:56:03 -0700

    Update example02.bash

--- eft-its/example02.bash
+++ eft-its/example02.bash
@@ -2,13 +2,7 @@
 fun_name_02(){
 ##TODO Catagory(submenu) ##A Short Decription of the Function 02
 
-  #save screen state
-  tput smcup
-  clear
-  echo -e "This has been an example exicution of a menu entry 02"
-  printf "\n\nPress any key to continue.\n"
-  read -n 1
-  #restore screen state
-  tput rmcup
+whiptail --msgbox "This has been an example exicution of a menu entry 02 \
+Press OK to continue" (stty size)
 
 }
